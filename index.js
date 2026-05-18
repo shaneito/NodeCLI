05e4d117bc391df5a8f7bf70b5d5eebeb62109ac
shaneito <44534398+shaneito@users.noreply.github.com>
2026-05-18 17:01:17 +0100

    📦 NEW: My Bio

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+console.log(`
+Shane Patel
+
+A hobbyist that is happy working in both Linux and MacOS.
+I enjoy trying out the latest software and tools be that Desktop Apps or the command
+line. I love coding, learning and creating new things whether it is Neovim or Awk.
+Currently learning JavaScript and Shell scripting to improve my workflow. If
+I am not doing that it is tweaking my system, either Kanata or my Neovim setup.
+
+🐤 Twitter: https://x.com/shaneito_on_x
+🗄️  GitHub:  https://github.com/shaneito
+🌐 Blog:    https://www.shaneito.me
+`);
